0ebf94d9bafbf2c5bdb9938f8db001c39b107758
pantherblack34 <89083500+pantherblack34@users.noreply.github.com>
2021-09-09 22:23:02 +0530

    taking input and updated css styling
    applied padding in button and margin.
    Taken the value inserted in textarea using js.

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const btnTranslate = document.querySelector("#btn-translate");
 
+var txtArea = document.querySelector("#text-area");
+
+console.log(txtArea);
+
+
+
+
 btnTranslate.addEventListener("click", function clicked() {
     console.log("clicked")
+    console.log("input", txtArea.value);
 });
 
