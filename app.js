4f653f8312ac5dd863c87600fcb6422144088131
pantherblack34 <89083500+pantherblack34@users.noreply.github.com>
2021-09-10 19:52:06 +0530

    edited every stuff and changed to original

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-var btnTranslate = document.querySelector("#btn-translate");
+let btnTranslate = document.querySelector("#btnTranslate");
+let inputTxt = document.querySelector("#txtInput");
+let outputTxt = document.querySelector("#outputDiv");
 
-var txtArea = document.querySelector("#text-area");
+let serverUrl = "https://api.funtranslations.com/translate/minion.json";
 
-var textArea = document.querySelector("#textarea");
+function getTranslationUrl(text) {
 
-function clicked() {
-    textArea.innerText = "asdfghjkl"+ " " + txtArea.value;
-};
+    return serverUrl + "?" + "text=" + text;
+}
 
-btnTranslate.addEventListener("click",clicked);
+function clickHandler(){
+
+    let inputText = inputTxt.value;
+
+    fetch(getTranslationUrl(inputText))
+        .then(response => response.json())
+        .then(json => {
+
+            let translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+
+        })
+}
+
+btnTranslate.addEventListener("click",clickHandler);
+
+
+
